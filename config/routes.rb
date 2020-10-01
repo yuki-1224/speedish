785a17ece925dd4ea5186c7f48a228aebc614be2
”takeyu” <youxizhn4116@gmail.com>
2020-10-01 23:26:20 +0900

    navigation create

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,11 @@ Rails.application.routes.draw do
   root 'pages#index'
   get 'users/new'
   
+  get 'sessions/new'
+
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
